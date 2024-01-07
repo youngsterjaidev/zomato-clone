a0af49f5bab38455c725d5be0203cbfb8409d555
youngsterjaidev <jaidevv999@gmail.com>
2024-01-07 14:11:13 +0530

    add files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios"
 import Navbar from "../components/Navbar";
 
 const Home = () => {
+  const [rname, setRname] = useState("")
+  const [result, setResult] = useState([])
+
+  const searchRestaurant = async (data) => {
+    try {
+      if(data === "") {
+        // show all data
+        const res = await axios.get(`http://10.200.189.21:8000/restaurants`)
+
+      console.log("The Response : ", res)
+
+      setResult(res.data)  
+        return
+      }
+
+      const res = await axios.get(`http://10.200.189.21:8000/restaurants/${data}`)
+
+      console.log("The Response : ", res)
+
+      setResult(res.data)
+    } catch(e) {
+      console.log("Error while searching the doc ")
+    }
+  }
+
+  useEffect(() => {
+    searchRestaurant("")
+  }, [])
+
   return (
     <>
       <Navbar />
@@ -17,9 +47,29 @@ const Home = () => {
               </datalist>
             </div>
             <div>
-              <input type="text" placeholder="Search for restauant" />
+              <input 
+                value={rname} 
+                type="text" 
+                placeholder="Search for restauant" 
+                onChange={e => {
+                  setRname(e.target.value)
+                  searchRestaurant(e.target.value)
+                }}
+                />
             </div>
           </form>
+
+          <ul>
+            {result.map(item => {
+              return (
+                  <li className="p-3 shadow-lg">
+                    <img src={item.imgdata} />
+                    <div>{item.rname}</div>
+                    <div>{item.price}</div>
+                  </li>
+              )
+            })}
+          </ul>
         </div>
       </div>
     </>
